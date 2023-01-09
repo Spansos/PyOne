4ed0361cf8b6ca6cbb2beecea5715839b9018132
Spansos <stan.spaans@student.hu.nl>
2023-01-09 17:23:14 +0100

    s

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-a = lambda *args: print(*args)
+linepath = [['main', 0]]
+prog = (({'main': 2}, None), ())
 
-a(10, 50)
+def run(path, prog):
+    n_path = path[1:]
+    if isinstance(n_path[0], int):
+        
+
+print(run(linepath, prog))
